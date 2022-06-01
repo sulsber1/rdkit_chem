9af66a88b991afcc4baf19d7d42856490b71c01a
Danny Sulsberger <sulsberd@oregonstate.edu>
2022-05-31 22:30:54 -0500

    removed validation check at the API level and moved it within the python objects within classy_kit.py

--- rd_smiles.py
+++ rd_smiles.py
@@ -2,7 +2,7 @@ from xmlrpc.client import Boolean
 from google.cloud import datastore
 from flask import Flask, render_template, request, redirect, session, make_response
 from rdkit import Chem
-import classy_kit
+import classy_kit, json
 
 
 app = Flask(__name__)
@@ -17,19 +17,20 @@ def index():
 def get_properties(smiles):
 
     if request.method == 'GET':
-        res = make_response(classy_kit.Molecule_obj(smiles).to_json())
-        res.headers.set('Content-Type', 'application/json')
-        if 'SMILES' in res.json['errors']:
-            err = "Error Parsing SMILES, try POST or review SMILES Submission"
-            return (err, 400)
-        else:
+        try:
+            res = make_response(classy_kit.Molecule_helper([smiles]).to_json())
+            res.headers.set('Content-Type', 'application/json')
             return (res, 200)
-
+        except:
+            return ('', 404)
     if request.method == 'POST':
-        content = request.get_json()
-        res = make_response(classy_kit.Molecule_helper(content["smiles"]).to_json())
-        res.headers.set('Content-Type', 'application/json')
-        return (res, 200)
+        try:
+            content = request.get_json()
+            res = make_response(classy_kit.Molecule_helper(content["smiles"]).to_json())
+            res.headers.set('Content-Type', 'application/json')
+            return (res, 200)
+        except:
+            return ('', 404)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
