fdd040aa515a3f9106e641b85a5b8b215bc42c89
Danny Sulsberger <sulsberd@oregonstate.edu>
2022-06-14 19:24:39 -0500

    added parallel branch

---
+++ rd_smiles_pl.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, make_response
+import classy_kit_pl
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/smiles', methods=['POST'])
+def get_properties():
+    if request.method == 'POST':
+        try:
+            content = request.get_json()
+            res = make_response(json.dumps(classy_kit_pl.get_descriptors(content)))
+            res.headers.set('Content-Type', 'application/json')
+            return (res, 200)
+        except:
+            return ('', 404)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
